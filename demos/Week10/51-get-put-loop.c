11772e3c150936e29aa5ab4c2961d74879bbc800
anishainas <anishainas@gmail.com>
2018-06-23 01:11:15 +0700

    added notes to program 51-get-put-loop.c in Week10

--- demos/Week10/51-get-put-loop.c
+++ demos/Week10/51-get-put-loop.c
@@ -15,3 +15,10 @@ void main (void) {
    }
 }
 
+/*
+ * getchar() reads a single character from the standard input stream and writes it to cc
+ * putchar(char) writes a character (an unsigned char) specified by the argument char to stdout
+ *
+ * this program will continue to read character from stdin until EOF, that is, no more input from a stream
+ * EOF is defined in stdio.h (and is usually -1). On linux, ctrl+d signals EOF, and on windows it's ctrl+z.
+ */
